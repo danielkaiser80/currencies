c67670b7a5a68d01ff6585c594d64b04f1e38c81
Daniel Kaiser <d.kaiser@wealthpilot.de>
2020-05-12 16:07:00 +0200

    initial commit (using Spring initializer)

---
+++ src/main/java/de/danielkaiser/currencies/CurrenciesApplication.java
@@ -0,0 +1,13 @@
+package de.danielkaiser.currencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrenciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrenciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/danielkaiser/currencies/CurrenciesApplicationTests.java
@@ -0,0 +1,14 @@
+package de.danielkaiser.currencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrenciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // empty as it just asserts the context to be loaded
+    }
+
+}
