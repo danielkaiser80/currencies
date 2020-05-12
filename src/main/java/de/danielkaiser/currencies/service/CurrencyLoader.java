131de752d0c14ea6e750c49449dd927b9b1af76a
Daniel Kaiser <d.kaiser@wealthpilot.de>
2020-05-12 17:00:47 +0200

    load the currencies from the ECB

--- src/main/java/de/danielkaiser/currencies/service/CurrencyLoader.java
+++ src/main/java/de/danielkaiser/currencies/service/CurrencyLoader.java
@@ -1,10 +1,22 @@
 package de.danielkaiser.currencies.service;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import de.danielkaiser.currencies.dto.CurrencyDto;
@@ -12,10 +24,51 @@ import de.danielkaiser.currencies.dto.CurrencyDto;
 @Service
 public class CurrencyLoader {
 
+    private static final String FILE_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip";
+    private static final String FILE_NAME = "eurofxref";
+    private static final String ZIP_FILE_NAME = FILE_NAME + ".zip";
+    private static final String CSV_FILE_NAME = FILE_NAME + ".csv";
+
+    private final Logger log = LogManager.getLogger();
+
     private Map<String, Double> currencies = new HashMap<>();
 
-
     public List<CurrencyDto> getAllCurrencies() {
+        // TODO trigger only when needed
+        loadCurrenciesFromEcb();
         return currencies.entrySet().stream().map(entry -> CurrencyDto.from(entry.getKey(), entry.getValue())).collect(Collectors.toList());
+    }
+
+    public void loadCurrenciesFromEcb() {
+
+        try (ReadableByteChannel readableByteChannel = Channels.newChannel(new URL(FILE_URL).openStream());
+                        FileOutputStream fileOutputStream = new FileOutputStream(ZIP_FILE_NAME)) {
+
+            fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+            Unzipper.unzipFile(ZIP_FILE_NAME);
+
+            buildMapFromFile();
+
+        } catch (IOException e) {
+            log.error(String.format("Could not retrieve ZIP file from %s", FILE_URL), e);
+        }
+    }
+
+    private void buildMapFromFile() {
+
+        try (Stream<String> stream = Files.lines(Paths.get(CSV_FILE_NAME))) {
+
+            final List<List<String>> currencyValues = stream.map(s -> s.split(",")).map(Arrays::asList).collect(Collectors.toList());
+            final List<String> isoCodes = currencyValues.get(0);
+
+            // first is date and last one is empty
+            currencies = IntStream.range(1, isoCodes.size() - 2).boxed().collect(Collectors.toMap(isoCodes::get, index -> {
+                final String s = currencyValues.get(1).get(index);
+                return Double.parseDouble(s);
+            }));
+
+        } catch (IOException e) {
+            log.error(String.format("Could not read CSV file from %s", CSV_FILE_NAME), e);
+        }
     }
 }
---
+++ src/main/java/de/danielkaiser/currencies/service/Unzipper.java
@@ -0,0 +1,56 @@
+package de.danielkaiser.currencies.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.lang.NonNull;
+
+public final class Unzipper {
+
+    private static final Logger log = LogManager.getLogger();
+
+    private Unzipper() {
+    }
+
+    public static void unzipFile(String fileName) {
+        File destDir = new File(".");
+        byte[] buffer = new byte[1024];
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(fileName))) {
+
+            ZipEntry zipEntry = zis.getNextEntry();
+            while (zipEntry != null) {
+                File newFile = newFile(destDir, zipEntry);
+                try (FileOutputStream fos = new FileOutputStream(newFile)) {
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                }
+                zipEntry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+        } catch (IOException e) {
+            log.error(String.format("Could not extract ZIP file %s", fileName), e);
+        }
+    }
+
+    @NonNull
+    public static File newFile(@NonNull File destinationDir, @NonNull ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+}
