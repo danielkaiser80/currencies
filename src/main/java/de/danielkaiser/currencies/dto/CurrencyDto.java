7a1865c4b61a57d9805f212cfd7e7b31fa7d19c5
Daniel Kaiser <daniel.kaiser@bridging-it.com>
2023-01-20 20:42:02 +0100

    Bump to Java 17 and Spring Boot 3.

--- src/main/java/de/danielkaiser/currencies/dto/CurrencyDto.java
+++ src/main/java/de/danielkaiser/currencies/dto/CurrencyDto.java
@@ -1,7 +1,7 @@
 package de.danielkaiser.currencies.dto;
 
 /**
- * A simple DTO, comprising of an ISO code and a currency value.
+ * A simple DTO, comprising an ISO code and a currency value.
  */
 public record CurrencyDto(String isoCode, Double value) {
 }
